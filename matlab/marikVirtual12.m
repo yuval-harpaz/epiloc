01c3ef4aa114ebb0732319c5493f5d01653a296a
oshrit <oshrit@meg-HP3.(none)>
2015-07-19 17:44:09 +0300

    tasks and comments

--- matlab/marikVirtual12.m
+++ matlab/marikVirtual12.m
@@ -1,21 +1,28 @@
+%% 
 nPNT=642;
 try
     cd /home/yuval/Data/marik/som2/1
-catch
-    cd /home/oshrit/MyDocuments/DATA/epiloc/data
+catch err
+    cd /home/oshrit/MyDocuments/DATA/som2/1
 end
 hs=ft_read_headshape('hs_file');
 hs=hs.pnt*1000;
 
 inward=[10 20 30];
 
+%% Creating sphearical grid with 3 layers - The grid has 920 points
+%% Layer 1
 [pnt1,lf1,vol]=sphereGrid(nPNT,inward(1)); % inner
+%% Unit vectors at each point
 pnt_complex1=findPerpendicular(pnt1);
+%% Layer 2
 [pnt2,lf2]=sphereGrid(nPNT,inward(2)); % outer
 pnt_complex2=findPerpendicular(pnt2);
+%% Layer 3
 [pnt3,lf3,vol]=sphereGrid(nPNT,inward(3)); % inner
 pnt_complex3=findPerpendicular(pnt3);
 pnt=[pnt1(:,:,1);pnt2(:,:,1);pnt3(:,:,1)];
+%% To cheack if inside head (ini = inside)
 ini=true(length(pnt),1);
 ini(pnt(:,3)<vol.o(3))=false; % keep only top half sphere
 ini(pnt(:,2)<min(hs(:,2)'+10))=false;
@@ -23,20 +30,34 @@ ini(pnt(:,2)>max(hs(:,2)'-10))=false;
 ini=find(ini);
 pnt=pnt(ini,:);
 
+%% xyz tangent1(anterior) tangent2 radial
 pnt_complex=pnt_complex1;
 pnt_complex(nPNT+1:nPNT*2,:,:)=pnt_complex2;
 pnt_complex(nPNT*2+1:nPNT*3,:,:)=pnt_complex3;
 pnt_complex=pnt_complex(ini,:,:);
 layer(1:nPNT,1)=1;layer(nPNT+1:nPNT*2)=2;layer(nPNT*2+1:nPNT*3)=3;
 layer=layer(ini);
-cd /home/yuval/Data/marik/som2/2
+
+%% Lead field (Foward solution of new vectors) 
+try
+    cd /home/yuval/Data/marik/som2/2
+catch err
+    cd /home/oshrit/MyDocuments/DATA/som2/2
+end
 [~,lf4]=sphereGrid(nPNT,inward(1));
 [~,lf5]=sphereGrid(nPNT,inward(2));
 [~,lf6]=sphereGrid(nPNT,inward(3));
-cd /home/yuval/Data/marik/som2/3
+
+try
+    cd /home/yuval/Data/marik/som2/3
+catch err
+    cd /home/oshrit/MyDocuments/DATA/som2/3
+end
 [~,lf7]=sphereGrid(nPNT,inward(1));
 [~,lf8]=sphereGrid(nPNT,inward(2));
 [~,lf9]=sphereGrid(nPNT,inward(3));
+
+%% Lead field of 3 Head positions (sensor positions)
 LF1=lf1; %head pos 1
 LF1.leadfield(nPNT+1:nPNT*2)=lf2.leadfield;
 LF1.leadfield(nPNT*2+1:nPNT*3)=lf3.leadfield;
@@ -74,11 +95,19 @@ plot3pnt(hs,'.')
 hold on
 plot3pnt(pnt,'.k')
 
-cd /home/yuval/Data/marik/som2
-load avgFilt
+%% Data with 3N channels (N=248) - Virtual Helmeat
+try
+    cd /home/yuval/Data/marik/som2/
+catch err
+    cd /home/oshrit/MyDocuments/DATA/som2/
+end
+load avgFilt % filtered data 1-20 Hz
 [data, cfg]=marikViUnite(avg1_handR,avg2_handR,avg3_handR);
 %LF.label=data.dataU.label
 
+%% gain matrix (2 sources for each point 920*2=1840), so we get the FW
+%% solution first for tangent1 and then for tanget2 - y
+%% in the dimenssions of 3N (virtual helmet) - x
 gain=[];
 for pnti=1:length(pnt)
     dip=(pnt_complex(pnti,:,2)-pnt_complex(pnti,:,1))*LF1.leadfield{pnti}';
@@ -90,38 +119,40 @@ for pnti=1:length(pnt)
     dip=[dip,(pnt_complex(pnti,:,3)-pnt_complex(pnti,:,1))*LF3.leadfield{pnti}'];
     gain(1:744,length(pnt)+pnti)=dip;
 end
+% figure; topoplot248(gain(1:248,1))
+% figure; topoplot248(gain(1:248,921))
+
 
 %% foot
-samp=180 ; % 145
+samp=180 ; % 145  %% specific time point
 [dataF, ~]=marikViUnite(avg1_footL,avg2_footL,avg3_footL);
 figure;plot(data.data1.time,data.data1.avg)
-MF=dataF.dataU.avg(:,samp);
+MF=dataF.dataU.avg(:,samp); % Meassuared Foot (Origina Data unite)
 
 topoplot248(MF(1:248))
 
-
-Pow=zeros(length(srci)*2,1);
+Pow=zeros(length(gain),1);
 tic
-for permi=1:100000
+for permi=1:1000
     Ran=[];
     
-    [~,ran]=sort(rand(1,length(srci)));
+    [~,ran]=sort(rand(1,length(gain)/2));
     selected=ran(1:5);
     Ran=[Ran;selected];
     
-    srcPerm=false(size(srci));
+    srcPerm=false(length(gain)/2,1);
     srcPerm(Ran)=true;
     Gain=gain(:,[srcPerm;srcPerm]);
-    source=Gain\MF;
-    recon=Gain*source;
-    R=corr(recon,MF).^100;
+    source=Gain\MF; % left divide source localization 
+    recon=Gain*source; % recounstruction
+    R=corr(recon,MF).^100; % Goodness of fit of reciunstracted vs. Meassured (Original - unite) 
     pow=zeros(size(Pow));
     pow([srcPerm;srcPerm])=source*R;
     Pow=Pow+pow;
     prog(permi)
 end
 toc
-Pow1=sqrt(Pow(1:920).^2+Pow(921:1840).^2);
+Pow1=sqrt(Pow(1:920).^2+Pow(921:1840).^2); % Power (vector magnitude)
 figure;
 scatter3pnt(pnt,25,Pow1)
 [~,maxPNT]=max(Pow1);
@@ -162,7 +193,6 @@ scatter3(pnt(maxPNT,1),pnt(maxPNT,2),pnt(maxPNT,3),30,0)
 sortPNT(1:5)
 
 %% one layers
-
 MHF248=MHF(1:248);%*2.5;
 Pow=zeros(length(srci)*2,1);
 tic
