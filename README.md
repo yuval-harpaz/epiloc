ecc921313e3262bab717bc3e4b89498d5abba4a9
yuval <yuval@DELL>
2016-08-07 21:00:26 +0300

    before submission 4

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 localizing epileptic MEG activity
 
 RIMDA is a tool for localizing an unknown number of dipoles.
-you can start by looking at the function rimda that was tested on 4D data.
+Please start by looking at rimda.m that was tested on 4D data.
 
 Dependencies:
-Grid Sphere matlab toolbox
+GridSphere matlab toolbox
 FieldTrip
--- matlab/rimda.m
+++ matlab/rimda.m
@@ -1,7 +1,22 @@
 function [PowCur,pnti,current,fwd]=rimda(M)
+% This function runs RIMDA source localization, finding dipoles for a
+% measured field M, a vector of 248 values in Tesla, the number of channels
+% for Magnes WH 3600 system.
+% We run the function in subject directory where the headshape can be found
+% (hs_file), along with data (c,rfhp0.1Hz) and the config file.
 
+% Output
+% current is the current at the chosen dipoles. pnti is the index
+% of the chosen points in the spheric grid. fwd is the reconstructed field based on these
+% sources.
+% PowCur needs fixing, it is for multiple timepoints, not applicable for
+% one time point.
+PowCur=[];
+% number of iterations
 N=10000;
-
+% set a spherical grid of equally distributed points (642). here we rely on
+% GridSphere, an external package, although FieldTrip creates such a grid
+% by default when no grid is specified for creating leadfield matrices.
 nPNT=642;
 hs=ft_read_headshape('hs_file');
 hs=hs.pnt*1000;
@@ -10,13 +25,17 @@ if exist('gain.mat','file')
     load gain
     load pnt
 else
-    [pnt1,lf1,vol]=sphereGrid(nPNT,inward(1)); % inner
+    % create 3 layers of 642 points spheres
+    % compute leadfields for all the points
+    [pnt1,lf1,vol]=sphereGridEL(nPNT,inward(1)); % outer
     pnt_complex1=findPerpendicular(pnt1);
-    [pnt2,lf2]=sphereGrid(nPNT,inward(2)); % outer
+    [pnt2,lf2]=sphereGridEL(nPNT,inward(2)); 
     pnt_complex2=findPerpendicular(pnt2);
-    [pnt3,lf3,vol]=sphereGrid(nPNT,inward(3)); % inner
-    pnt_complex3=findPerpendicular(pnt3);
+    [pnt3,lf3,vol]=sphereGridEL(nPNT,inward(3)); % inner
+    pnt_complex3=findPerpendicular(pnt3); % use two vectors per dipole location
     pnt=[pnt1(:,:,1);pnt2(:,:,1);pnt3(:,:,1)];
+    % find indices for points inside the headshape, top half of the
+    % spheres. cut points outside both ears.
     ini=true(length(pnt),1);
     ini(pnt(:,3)<vol.o(3)-20)=false; % keep only top half sphere
     ini(pnt(:,2)<min(hs(:,2)'+10))=false;
@@ -27,6 +46,7 @@ else
     pnt_complex(nPNT+1:nPNT*2,:,:)=pnt_complex2;
     pnt_complex(nPNT*2+1:nPNT*3,:,:)=pnt_complex3;
     pnt_complex=pnt_complex(ini,:,:);
+    % index for depth of points
     layer(1:nPNT,1)=1;layer(nPNT+1:nPNT*2)=2;layer(nPNT*2+1:nPNT*3)=3;
     layer=layer(ini);
     LF=lf1;
@@ -39,6 +59,7 @@ else
     LF.inside(nPNT+1:nPNT*2)=lf2.inside;
     LF.inside(nPNT*2+1:nPNT*3)=lf2.inside;
     LF.inside=LF.inside(ini);
+    % making a gain matrix for all the points
     gain=[];
     for pnti=1:length(pnt)
         dip=(pnt_complex(pnti,:,2)-pnt_complex(pnti,:,1))*LF.leadfield{pnti}';
@@ -50,33 +71,45 @@ else
     save layer layer
     save gain gain
 end
-%% 
+%% RIMDA stage
+% run permutations: select 10 locations in each run of the loop and check
+% their possible contribution to the measured field M.
 Npnt=length(pnt);
+% prepare an empty vector Pow to store the results of all the permutations
 Pow=zeros(2*Npnt,size(M,2));
+% loop fo N times (10000)
 for permi=1:N
+    % choose 10 locations
     Ran=[];
     [~,ran]=sort(rand(1,Npnt));
     selected=ran(1:10);
     Ran=[Ran;selected];
     srcPerm=false(1,Npnt);
     srcPerm(Ran)=true;
+    % use the gain matrix of the chosen 10 points only
     Gain=gain(:,[srcPerm,srcPerm]);
-    source=Gain\M;
-    recon=Gain*source;
-    R=corr(recon(:),M(:)).^100;
-    pow=zeros(size(Pow,1),size(M,2));
-    pow([srcPerm,srcPerm],:)=source.*R;
-    Pow=Pow+pow;
-    prog(permi)
+    source=Gain\M; % left divide (similar to pinv)
+    recon=Gain*source; % reconstruct the field from hypothesised sources
+    R=corr(recon(:),M(:)).^100; % compute correlation between measured and reconstructed field
+                                % use power of 100 to supress solutions
+                                % that have less than excellent fit
+    pow=zeros(size(Pow,1),size(M,2)); 
+    pow([srcPerm,srcPerm],:)=source.*R; % weight the sources by r^100
+    Pow=Pow+pow;    % sum the result with previous ones
+    prog(permi) % feedback on progress
 end
 
-
-%PowSim=sqrt(Pow(1:Npnt).^2+Pow(Npnt+1:2*Npnt).^2);
-%figure;scatter3pnt(pnt,25,PowSim)
+%% Choose sources
+% this function evaluates which sources are 'local maxima' within a radius of  
+% 30mm, and pass threshold of at least 0.3 strength compared to the strongest source.
+% it returns current for sources that are strong maxima. pnti is the index
+% of the chosen points. fwd is the reconstructed field based on these
+% sources.
 [current,~,pnti,fwd]=getCurrent2(Pow,pnt,M,gain,30,0.3,false);
-%[current,~,pnti,~,fwd]=getCurrent(Pow,pnt,M,gain,30,0.3,false);
-if size(M,2)==1
-    PowCur=zeros(Npnt,1);
+
+% make some plots and prepare output
+if size(M,2)==1 % for localizing more than one time sample
+    PowCur=zeros(Npnt,1); 
     PowCur(pnti)=mean(current,2);
     cfg=[];
     cfg.interactive='no';
